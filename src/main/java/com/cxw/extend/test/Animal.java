c251cc1052e10dff39f04086e17d2999c6cc6398
hoxiyigu1 <hoxiyigu@163.com>
2017-03-02 17:29:16 +0800

    test

---
+++ src/main/java/com/cxw/extend/test/Animal.java
@@ -0,0 +1,7 @@
+package com.cxw.extend.test;
+
+public class Animal {
+	public void test(){
+		int a = 10;
+	}
+}
---
+++ src/main/java/com/cxw/extend/test/Dog.java
@@ -0,0 +1,11 @@
+package com.cxw.extend.test;
+
+public class Dog extends Animal{
+	int a;
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		super.test();
+	}
+	
+}
